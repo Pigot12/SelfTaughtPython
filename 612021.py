8396546f9ed2ba8a789aa84c7df674997d576b6d
Pigot12 <45109176+Pigot12@users.noreply.github.com>
2021-01-06 21:09:24 +0200

    Boolean Values Practice

---
+++ 612021.py
@@ -0,0 +1,59 @@
+#Boolean values=Logikes times
+
+age = 25
+
+age >= 20
+
+year = 2015
+year <= 2010
+
+#and=kai(otan baloume 2 synuhkes prepei na isxyoun kai oi 2 gia na exoume true)
+
+year = 2035 
+year >= 2015 and year <= 2045
+
+year = 2005
+
+#or=h(otan baloume 2 synuhkes prepei na isxyoun h 1 gia na exoume true )
+
+year = 2005
+year < 2015 or year > 2000
+
+year = 2015
+year < 2005 or year < 2000
+
+year = 2005
+(year == 2005 and year < 2035) or 0 > year
+
+year = 2005
+(year == 2005 and year < 2035) or 25 > year
+
+year = 2005
+(year == 2015 and year < 2035) or 25 > year
+
+year = 2005
+(year == 2015 or year < 2035) and 2015 > year
+
+year = -2005
+(year == 2015 or year < 2035) and 2015 > year
+
+#==    =Kati isoute me kati allo
+
+#=  =Orizoyme metablites 
+
+year = -2005
+
+year == -2005
+
+#!=     =Den isoutai.Ginetai otan eixame balei gia metablith prin mia timh kai twra den thn ueloume
+#Thn kanoume kati allo kai bgazei true
+
+2005 != 2005
+
+year != -2005
+
+year != 2006
+
+year != 2010
+
+20 != 20
